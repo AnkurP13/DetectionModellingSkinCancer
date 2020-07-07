4aa578c894d4364461780ba3c8db5f4d54908b9e
AnkurP13 <33151447+1619450@users.noreply.github.com>
2020-07-07 16:48:26 +0100

    Implemented Maps Page

--- app/src/main/java/com/example/detectionmodellingskincancer/Map.java
+++ app/src/main/java/com/example/detectionmodellingskincancer/Map.java
@@ -1,14 +1,244 @@
 package com.example.detectionmodellingskincancer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 public class Map extends AppCompatActivity {
+
+    //Variables
+    Spinner spType;
+    Button btFind;
+    SupportMapFragment supportMapFragment;
+    GoogleMap map;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat = 0, currentLong = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        //Assign Variables
+        spType = findViewById(R.id.sp_type);
+        btFind = findViewById(R.id.bt_find);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+
+        //initialze the array of places
+        final String []placeTypeList = {"hospital","practice"};
+        //Initialise the array of places name
+        String[] placeNameList = {"hospital","practice"};
+
+        //Set the adapted on a spinner
+        spType.setAdapter(new ArrayAdapter<>(Map.this,
+                android.R.layout.simple_spinner_dropdown_item,placeNameList));
+
+        //Initialize fused
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //check for permission
+        if(ActivityCompat.checkSelfPermission(Map.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //When Permission granted
+            //call method
+            getCurrentLocation();
+        }   else {
+            //when permission is denied
+            //request Permission
+            ActivityCompat.requestPermissions(Map.this
+                    ,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44) ;
+        }
+
+        btFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //GET Selected position of spinner
+                int i = spType.getSelectedItemPosition();
+                //initialise url
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +//url
+                        "?location=" + currentLat + "," + currentLong + //location latitude and longitutfe
+                        "&types=" + placeTypeList[i] + //place type
+                        "&sensor=true" + //sensor
+                        "&key=" + getResources().getString(R.string.notmyapi); //Google map key
+
+                //execute place task method to download json data
+                new PlaceTask().execute(url);
+            }
+        });
+
+
+    }
+
+    private void getCurrentLocation() {
+        //Intialize task location
+        Task<Location> task  = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //when success
+                if(location!= null){
+                    //when location is not equal to null
+                    //get current lattitude
+                    currentLat = location.getLatitude();
+                    //get current longitude
+                    currentLong = location.getLongitude();
+                    //sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //when map is ready
+                            map = googleMap;
+                            //zoom current map location
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom
+                                    (new LatLng(currentLat, currentLong), 10 ));
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44){
+            if (grantResults.length> 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                //when permission
+                //call method
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                //intitialze data
+                data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+          //execute parser task
+            new ParserTask().execute(s);
+        }
+    }
+
+
+    private String downloadUrl(String string) throws IOException {
+        //iNITIALIZE URL
+        URL url = new URL(string);
+        //initialize connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        //connection connectiuon
+        connection.connect();
+        //initizle input strema
+        InputStream stream = connection.getInputStream();
+        //Initialize buffer reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        //Initilialize strign variable
+        String line = "";
+        //use while loop
+        while ((line = reader.readLine())!= null){
+            //append line
+            builder.append(line);
+        }
+        //get append data
+        String data = builder.toString();
+        //close reader
+        reader.close();
+        //return data
+        return data;
+
+
+    }
+
+    private class ParserTask extends AsyncTask<String,Integer, List<HashMap<String,String>>> {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            //create json Parser Class
+            JsonParser jsonParser = new JsonParser();
+            //initialize hash map list
+            List<HashMap<String, String>> mapList = null;
+            JSONObject object = null;
+            try {
+                //Initlize json object
+                object = new JSONObject(strings[0]);
+                //parse json object
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            //clear map
+            map.clear();
+            //use for lopp
+            for(int i=0; i<hashMaps.size();i++){
+                //intitize hash map
+                HashMap<String,String> hashMapList = hashMaps.get(i);
+                //get latitude
+                double lat = Double.parseDouble(hashMapList.get("lat"));
+                //get longitude
+                double lng = Double.parseDouble(hashMapList.get("lng"));
+                //get name
+                String name = hashMapList.get("name");
+                //concate latitude and longitude
+                LatLng latLng = new LatLng(lat,lng);
+                //initialize marker options
+                MarkerOptions options = new MarkerOptions();
+                //set postiion
+                options.position(latLng);
+                //set title
+                options.title(name);
+                //add marker on map
+                map.addMarker(options);
+            }
+        }
     }
 }
